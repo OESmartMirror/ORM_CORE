bd32a2fde7c5019f79a0d267987fb4c719b5db85
András Tóth <csonti04@example.com>
2019-10-13 18:00:25 +0200

    Initial commit.

---
+++ ORM_Core/Start.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Core
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            datatableEntities1 datatableEntities1 = new datatableEntities1();
+            datatableEntities1.users.
+        }
+    }
+}
