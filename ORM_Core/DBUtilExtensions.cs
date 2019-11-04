3e01dc16a5a4302a3be491a2ba0838d0256b48de
András Tóth <csonti04@example.com>
2019-11-04 23:50:18 +0100

    Developing further capabilities with extension class

--- ORM_Core/DBUtilExtensions.cs
+++ ORM_Core/DBUtilExtensions.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ORM_Core
 {
     public static class DBUtilExtensions
     {
+        //TODO lehet kellene explicit használni az id-t, hogy ne kelljen n*n-es keresést csinálni a programokra
+        //TODO vagy alkalmazni egy csak konstansokat tartalmazó osztályt
         public static void AddPicture(this user _user, picture _picture)
         {
             if (!_user.pictures.Contains(_picture))
@@ -51,7 +56,20 @@ namespace ORM_Core
             }
         }
 
-        public static void AddParameter(this user _user, List<users_parameters> _parameters)
+        public static void AddParameter(this user _user, String _paramName, String _paramValue)
+        {
+            users_parameters temp = new users_parameters
+            {
+                parameter_name = _paramName,
+                parameter_value = _paramValue
+            };
+            if (!_user.users_parameters.Contains(temp))
+            {
+                _user.users_parameters.Add(temp);
+            }
+        }
+
+        public static void AddParameters(this user _user, List<users_parameters> _parameters)
         {
             foreach (users_parameters param in _parameters)
             {
@@ -81,6 +99,116 @@ namespace ORM_Core
             }
         }
 
+        public static void AddProgram(this user _user, String _programName)
+        {
+            program temp = new program();
+            temp.AddParameters("ProgramName",_programName);
+            _user.programs.Add(temp);
+        }
 
+        public static void AddParameters(this program _program, program_parameters _parameter)
+        {
+            if(!_program.program_parameters.Contains(_parameter))
+            {
+                _program.program_parameters.Add(_parameter);
+            }
+        }
+
+        public static void AddParameters(this program _program, String _paramName, String _paramValue)
+        {
+            program_parameters temp = new program_parameters
+            {
+                parameter_name = _paramName,
+                parameter_value = _paramValue
+            };
+            if (!_program.program_parameters.Contains(temp))
+            {
+                _program.program_parameters.Add(temp);
+            }
+        }
+
+        public static void AddParameters(this program _program, List<program_parameters> _parameters)
+        {
+            foreach (program_parameters param in _parameters)
+            {
+                if (!_program.program_parameters.Contains(param))
+                {
+                    _program.program_parameters.Add(param);
+                }
+            }
+        }
+
+        public static String GetParametersAsJson(this user _user)
+        {
+            return JsonConvert.SerializeObject(_user.users_parameters);
+        }
+
+        public static String GetParametersAsJson(this program _program)
+        {
+            return JsonConvert.SerializeObject(_program.program_parameters);
+        }
+
+        public static String GetProgramsAsJson(this user _user)
+        {
+            return JsonConvert.SerializeObject(_user.programs);
+        }
+
+        public static void ExportUserParametersAsJson(this user _user, String _path)
+        {
+            string correspondingUserPath = _path + _user.label + Path.DirectorySeparatorChar;
+            Directory.CreateDirectory(Path.GetDirectoryName(correspondingUserPath));
+            using (StreamWriter file = File.CreateText(correspondingUserPath + Path.DirectorySeparatorChar + _user.label + "_user_parameters.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, _user.GetParametersAsJson());
+            }
+        }
+
+        public static void ExportProgramParametersAsJson(this user _user, String _path)
+        {
+            string correspondingUserPath = _path + _user.label + Path.DirectorySeparatorChar;
+            Directory.CreateDirectory(Path.GetDirectoryName(correspondingUserPath));
+            using (StreamWriter file = File.CreateText(correspondingUserPath + Path.DirectorySeparatorChar + _user.label + "_program_parameters.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, _user.GetProgramsAsJson());
+            }
+        }
+
+        public static void ExportUserImages(this user _user, String _path)
+        {
+            string correspondingUserPath = _path + _user.label + Path.DirectorySeparatorChar;
+            //Directory.CreateDirectory(Path.GetDirectoryName(correspondingUserPath));
+            //using (StreamWriter file = File.CreateText(correspondingUserPath + Path.DirectorySeparatorChar + _user.label + ".jpeg"))
+            //{
+            //    JsonSerializer serializer = new JsonSerializer();
+            //    serializer.Serialize(file, _user.GetProgramsAsJson());
+            //}
+            foreach(picture pic in _user.pictures)
+            {
+                MemoryStream ms = new MemoryStream(pic.picture1);
+                System.Drawing.Image img = System.Drawing.Image.FromStream(ms);
+                img.Save(correspondingUserPath + Path.DirectorySeparatorChar + _user.label + "_" + pic.picture1.ToString().GetHashCode() + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+        }
+
+        public static program GetProgramByName(this user _user, String _programName)
+        {
+            //TODO fix this shit
+            program temp = null;
+            foreach(program prog in _user.programs)
+            {
+                foreach(program_parameters pp in prog.program_parameters)
+                {
+                    if(pp.parameter_name.Equals("ProgramName") && pp.parameter_value.Equals(_programName))
+                    {
+                        temp = prog;
+                    }
+                }
+
+                //TODO Figure out how change to .net 4.8
+            }
+            return temp;
+        }
     }
 }
--- ORM_Core/Start.cs
+++ ORM_Core/Start.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,19 @@ namespace ORM_Core
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //Console.WriteLine("Hello World!");
+            //Console.ReadKey();
             datatableEntities1 datatableEntities1 = new datatableEntities1();
             //datatableEntities1.users.
-
+            
+            String path = "C:" + Path.DirectorySeparatorChar + "Face" + Path.DirectorySeparatorChar;
             user user = new user();
+            user.AddParameter("Name","Tóth András");
+            user.AddParameter("Birthday","1991.06.18");
+            user.AddProgram("Weather");
+            user.GetProgramByName("Weather").AddParameters("Location", "Székesfehérvár");
+            datatableEntities1.users.Add(user);
+            datatableEntities1.SaveChanges();
         }
     }
 }
